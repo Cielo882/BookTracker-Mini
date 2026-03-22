8bf7654f1d1e24dd227730f26f75f7b6dfb64704
cielo882 <cielo882@gmail.com>
2026-03-21 20:09:20 -0600

    Se genera version de pruebas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.cielo.applibros"
         minSdk =28
         targetSdk = 35
-        versionCode =4
-        versionName ="2.2"
+        versionCode =5
+        versionName ="2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
